817e6cca1d4c163705c4a2a340c49b94da0b682a
techmohit251299-eng <techmohit251299@gmail.com>
2026-07-31 16:59:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 if GEMINI_API_KEY:
     genai.configure(api_key=GEMINI_API_KEY)
 
-model = genai.GenerativeModel("gemini-2.5-flash")
+model = genai.GenerativeModel("gemini-flash-latest")
 
 
 class TripRequest(BaseModel):
